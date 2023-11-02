ac1941f8f661b2be0ebc9c5c00986ebdadf4e30c
Sampo Westerholm <sampo.westerholm@gmail.com>
2023-11-02 17:36:56 +0200

    Changes

--- src/main/java/com/example/westerholmbookstore/WesterholmBookStoreApplication.java
+++ src/main/java/com/example/westerholmbookstore/WesterholmBookStoreApplication.java
@@ -36,10 +36,10 @@ public class WesterholmBookStoreApplication {
             repository.save(new Book("Paul", "Jokela", "2006","ISB5939595", "13,44€", crepository.findByName("History").get(0)));
             
             // Create users: admin/admin user/user
-            AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@haagahelia.fi");
-            AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ROLE_ADMIN", "admin@haagahelia.fi");
-            urepository.save(user1);
-            urepository.save(user2);
+            //AppUser user1 = new AppUser("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@haagahelia.fi");
+            //AppUser user2 = new AppUser("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ROLE_ADMIN", "admin@haagahelia.fi");
+           // urepository.save(user1);
+         //   urepository.save(user2);
             
             log.info("fetch all Books");
             for (Book book : repository.findAll()) {
